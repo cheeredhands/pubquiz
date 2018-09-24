15b31321f529e87f3598f39e53ef0947b7f79912
Mark Achten <mark.achten@simulationcrew.com>
2018-09-24 21:02:15 +0200

    test

--- Pubquiz.WebApi/Controllers/ValuesController.cs
+++ Pubquiz.WebApi/Controllers/ValuesController.cs
@@ -14,6 +14,7 @@ namespace Pubquiz.WebApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        // gtest
         private readonly IRepositoryFactory _repositoryFactory;
 
         public ValuesController(IRepositoryFactory repositoryFactory)
