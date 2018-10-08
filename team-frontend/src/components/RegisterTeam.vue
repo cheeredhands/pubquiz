1a148236daa3de9022a31ded2745ecbcbec7f1ad
tjeerdhans <tjeerdhans.terpstra@cito.nl>
2018-10-08 22:56:06 +0200

    Initial team frontend!

---
+++ team-frontend/src/components/RegisterTeam.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="register">
+    <h1>{{ msg }}</h1>
+  <p><label for="teamName">Team name</label><input v-model="teamName" id="teamName" /> </p> 
+ <p> <label for="code">Code</label> <input v-model="code" id="code"/></p>
+ <p><button type="submit" @click="register()">Register</button></p>
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+
+export default {
+  name: "RegisterTeam",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      teamName: "",
+      code: ""
+    };
+  },
+  methods: {
+    register() {
+      // register!
+      Axios.post(
+        "http://localhost:5000/api/account/register",
+        {
+          teamName: this.teamName,
+          code: this.code
+        },
+        { withCredentials: true }
+      )
+        .then(response => (this.msg = response.data.teamId))
+        .catch(error => (this.msg = error.response.data[0].message));
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
