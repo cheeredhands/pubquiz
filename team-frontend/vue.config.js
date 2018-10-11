2c2ddef9bc6103344aa7e2d610a491837e6130d9
Mark Achten <mark.achten.88@gmail.com>
2018-10-11 22:04:31 +0200

    Added proxy configuration and most likely also a more convenient way of managing urls

--- team-frontend/vue.config.js
+++ team-frontend/vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-    configureWebpack: {
-      devtool: 'source-map'
+  configureWebpack: {
+    devtool: 'source-map'
+  },
+  devServer: {
+    proxy: {
+      // setup a proxy indicating all calls made to /api...
+      // should be made to http://localhost:5000.
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
     }
   }
+}
