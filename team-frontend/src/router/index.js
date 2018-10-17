dac2a995c7396cd8e2bf67f08376741fa4b2d808
Tjeerd Hans Terpstra <tjeerdhans.terpstra@cito.nl>
2018-10-17 21:55:25 +0200

    typo

--- team-frontend/src/router/index.js
+++ team-frontend/src/router/index.js
@@ -14,7 +14,7 @@ export default new Router({
     component: Home
   },{
     path: '/register',
-    name: 'Regisyter',
+    name: 'Register',
     component: RegisterTeam
   },{
     path: '/lobby',
