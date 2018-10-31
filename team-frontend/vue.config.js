db53c60562111ab6dbeef08a078f26ded1e3201e
tjeerdhans <tjeerdhans@gmail.com>
2018-10-31 21:38:08 +0100

    updated deps

--- team-frontend/vue.config.js
+++ team-frontend/vue.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   configureWebpack: {
-    devtool: 'source-map'
+    devtool: "source-map"
   },
   devServer: {
     proxy: {
       // setup a proxy indicating all calls made to /api...
       // should be made to http://localhost:5000.
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true
       }
     }
   }
-}
+};
