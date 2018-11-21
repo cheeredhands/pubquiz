75cb69997a0225081c0d2aadb01870ff90601770
markachten <mark.achten.88@gmail.com>
2018-11-21 21:28:17 +0100

    Added TeamId to returning TeamViewModel

--- backend/Pubquiz.Logic/Requests/TeamLobbyViewModelQuery.cs
+++ backend/Pubquiz.Logic/Requests/TeamLobbyViewModelQuery.cs
@@ -25,7 +25,7 @@ namespace Pubquiz.Logic.Requests
         {
             var teamCollection = UnitOfWork.GetCollection<Team>();
             var team = await teamCollection.GetAsync(TeamId);
-           var gameCollection = UnitOfWork.GetCollection<Game>();
+            var gameCollection = UnitOfWork.GetCollection<Game>();
             var game = await gameCollection.GetAsync(team.GameId);
 
             if (game.State != GameState.Open)
@@ -36,12 +36,18 @@ namespace Pubquiz.Logic.Requests
 
             var otherTeamsInGame = teamCollection.AsQueryable()
                 .Where(t => t.Id != TeamId && game.TeamIds.Contains(t.Id))
-                .Select(t => new TeamViewModel {TeamName = t.Name, MemberNames = t.MemberNames}).ToList();
+                .Select(t => new TeamViewModel
+                {
+                    TeamId = t.Id,
+                    TeamName = t.Name, 
+                    MemberNames = t.MemberNames
+                })
+                .ToList();
 
             var model = new TeamLobbyViewModel
             {
                 TeamId = TeamId,
-                Team = new TeamViewModel {TeamName = team.Name, MemberNames = team.MemberNames},
+                Team = new TeamViewModel { TeamName = team.Name, MemberNames = team.MemberNames},
                 OtherTeamsInGame = otherTeamsInGame
             };
             return model;
