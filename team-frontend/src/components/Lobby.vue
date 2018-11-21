10249b46a0a2c703c1562fda7a088cee8605970b
markachten <mark.achten.88@gmail.com>
2018-11-21 21:40:10 +0100

    Double quotes

--- team-frontend/src/components/Lobby.vue
+++ team-frontend/src/components/Lobby.vue
@@ -11,32 +11,32 @@
       </b-input-group-append>
       </b-input-group>
     </b-form>
-    <!-- <input v-if='isInEdit' v-model='newName' id='teamName' />
+    <!-- <input v-if="isInEdit" v-model="newName" id="teamName" />
     <div v-else>
       {{ team.teamName }}
     </div> -->
     <div>
       Geef hier de namen van je teamleden op (een per regel): <br/>
-      <textarea rows='4' cols='30' v-model='memberNames'></textarea>
-      <button @click='saveMembers()'>Opslaan</button>
+      <textarea rows="4" cols="30" v-model="memberNames"></textarea>
+      <button @click="saveMembers()">Opslaan</button>
     </div>
    <hr />
     <p>Jullie gaan het opnemen tegen:</p>
     <ul>
-      <li v-for='(team, index) in otherTeams' :key='index'>
+      <li v-for="(team, index) in otherTeams" :key="index">
         {{ team.teamName }}
       </li>
     </ul>
 </div>
 </template>
 
-<script lang='ts'>
-import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
-import { Route } from 'vue-router';
-import store from '../store';
-import { AxiosResponse, AxiosError } from 'axios';
-import { TeamLobbyViewModel, ApiResponse } from '../models/models';
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import { Route } from "vue-router";
+import store from "../store";
+import { AxiosResponse, AxiosError } from "axios";
+import { TeamLobbyViewModel, ApiResponse } from "../models/models";
 
 @Component({
   beforeRouteEnter(to: Route, from: Route, next: any) {
@@ -45,22 +45,22 @@ import { TeamLobbyViewModel, ApiResponse } from '../models/models';
     // because it has not been created yet when this guard is called!
 
     if (!store.state.isLoggedIn) {
-      next('/');
+      next("/");
     }
     // todo also check the state of the game, you might want to go straight back into the game.
     next();
   }
 })
 export default class Lobby extends Vue {
-  public name: string = 'Lobby';
+  public name: string = "Lobby";
 
   public inEdit: boolean = false;
 
-  public newName: string = '';
+  public newName: string = "";
 
-  public teamId: string = '';
+  public teamId: string = "";
 
-  public memberNames: string = '';
+  public memberNames: string = "";
 
   public created() {
     this.newName = this.team.teamName;
@@ -69,10 +69,10 @@ export default class Lobby extends Vue {
 
     // get team lobby view model
     this.$axios
-      .get('/api/game/teamlobby')
+      .get("/api/game/teamlobby")
       .then((response: AxiosResponse<TeamLobbyViewModel>) => {
-        this.$store.commit('setTeam', response.data.team);
-        this.$store.commit('setOtherTeams', response.data.otherTeamsInGame);
+        this.$store.commit("setTeam", response.data.team);
+        this.$store.commit("setOtherTeams", response.data.otherTeamsInGame);
       })
       .catch((error: AxiosError) => {
         this.$snotify.error(error.message);
@@ -81,7 +81,7 @@ export default class Lobby extends Vue {
 
   public saveMembers() {
     this.$axios
-      .post('api/account/changeteammembers', {
+      .post("api/account/changeteammembers", {
         teamId: this.team.teamId,
         teamMembers: this.memberNames
       })
@@ -97,13 +97,13 @@ export default class Lobby extends Vue {
     // call api that team name changed but only if team name has not changed!
     if (this.team.teamName !== this.newName) {
       this.$axios
-        .post('/api/account/changeteamname', {
+        .post("/api/account/changeteamname", {
           teamId: this.teamId,
           newName: this.newName
         })
         .then((response: AxiosResponse<ApiResponse>) => {
           // only save it to the store if api call is successful!
-          this.$store.commit('setOwnTeamName', this.newName);
+          this.$store.commit("setOwnTeamName", this.newName);
           this.$snotify.success(response.data.message);
         })
         .catch((error: AxiosError) => {
@@ -113,7 +113,7 @@ export default class Lobby extends Vue {
   }
 
   get team() {
-    return this.$store.state.team || '';
+    return this.$store.state.team || "";
   }
 
   get otherTeams() {
