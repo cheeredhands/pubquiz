45800cdf6fd3d2ad6ca65bae897b69a858af551e
Mark Achten <mark.achten.88@gmail.com>
2019-10-19 22:51:57 +0200

    Removed missed out ref to vue-notify

--- team-frontend/src/plugins/index.d.ts
+++ team-frontend/src/plugins/index.d.ts
@@ -1,6 +1,5 @@
 import Vue, { VueConstructor } from 'vue';
 import { AxiosInstance } from 'axios';
-import { SnotifyService } from 'vue-snotify/SnotifyService';
 import { QuizrHelpers } from './quizr-helpers';
 
 declare global {
@@ -12,12 +11,10 @@ declare global {
 declare module 'vue/types/vue' {
   interface Vue {
     $axios: AxiosInstance;
-    $snotify: SnotifyService;
     $quizrhelpers: QuizrHelpers;
   }
   interface VueConstructor {
     $axios: AxiosInstance;
-    $snotify: SnotifyService;
     $quizrhelpers: QuizrHelpers;
   }
 }
