3623fadbacc8e08f0e57db3abfc9f29872d240b3
Mark Achten <mark.achten.88@gmail.com>
2019-10-28 22:23:16 +0100

    Did home.vue

--- team-frontend/src/components/Home.vue
+++ team-frontend/src/components/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="content">
-      <h1>Welkom bij Quizr!</h1>
-      <router-link class="btn btn-primary btn-lg" :to="{name: 'RegisterTeam', params: { msg: 'Hello new team!' } }">Registreer hier</router-link>
-      <hr>
-      <router-link :to="{name: 'QuizMasterLogin'}">Quizmasters</router-link>
+    <h1>{{ $t('components.home.WELCOME')}}</h1>
+    <router-link
+      class="btn btn-primary btn-lg"
+      :to="{name: 'RegisterTeam'}">{{ $t('components.home.REGISTERHERE') }}</router-link>
+    <hr />
+    <router-link :to="{name: 'QuizMasterLogin'}">{{ $t('components.home.QUIZMASTERLOGIN') }}</router-link>
   </div>
 </template>
 
-<style scoped>
+<style scoped>
 </style>
+
