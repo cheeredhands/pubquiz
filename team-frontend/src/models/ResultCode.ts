c65ade20e4749ef17558698a660db840402bec97
Mark Achten <mark.achten.88@gmail.com>
2020-05-24 15:15:34 +0200

    Marked unused enum as comment

--- team-frontend/src/models/ResultCode.ts
+++ team-frontend/src/models/ResultCode.ts
@@ -1,26 +1,26 @@
-export enum ResultCode {
-    /* Error codes */
-    InvalidCode = 'InvalidCode',
-    TeamNameIsTaken = 'TeamNameIsTaken',
-    InvalidTeamId = 'InvalidTeamId',
-    InvalidQuestionId = 'InvalidQuestionId',
-    QuestionNotInQuiz = 'QuestionNotInQuiz',
-    QuestionNotInCurrentQuizSection = 'QuestionNotInCurrentQuizSection',
-    InvalidInteractionId = 'InvalidInteractionId',
-    InvalidGameStateTransition = 'InvalidGameStateTransition',
-    InvalidGameId = 'InvalidGameId',
-    InvalidUserNameOrPassword = 'InvalidUserNameOrPassword',
-    InvalidUserId = 'InvalidUserId',
-    QuizMasterUnauthorizedForGame = 'QuizMasterUnauthorizedForGame',
-    LobbyUnavailableBecauseOfGameState = 'LobbyUnavailableBecauseOfGameState',
-    NoRoleClaimForUser = 'NoRoleClaimForUser',
-    NoCurrentGameIdClaimForUser = 'NoCurrentGameIdClaimForUser',
-    UnauthorizedRole = 'UnauthorizedRole',
-    TeamAlreadyLoggedIn = 'TeamAlreadyLoggedIn',
-    InvalidItemNavigation = 'InvalidItemNavigation',
-    ValidationError = 'ValidationError',
-    /* Success codes */
-    Ok = 'Ok',
-    ThatsYou = 'ThatsYou',
-    LoggedOut = 'LoggedOut'
-}
+// export enum ResultCode {
+//     /* Error codes */
+//     InvalidCode = 'InvalidCode',
+//     TeamNameIsTaken = 'TeamNameIsTaken',
+//     InvalidTeamId = 'InvalidTeamId',
+//     InvalidQuestionId = 'InvalidQuestionId',
+//     QuestionNotInQuiz = 'QuestionNotInQuiz',
+//     QuestionNotInCurrentQuizSection = 'QuestionNotInCurrentQuizSection',
+//     InvalidInteractionId = 'InvalidInteractionId',
+//     InvalidGameStateTransition = 'InvalidGameStateTransition',
+//     InvalidGameId = 'InvalidGameId',
+//     InvalidUserNameOrPassword = 'InvalidUserNameOrPassword',
+//     InvalidUserId = 'InvalidUserId',
+//     QuizMasterUnauthorizedForGame = 'QuizMasterUnauthorizedForGame',
+//     LobbyUnavailableBecauseOfGameState = 'LobbyUnavailableBecauseOfGameState',
+//     NoRoleClaimForUser = 'NoRoleClaimForUser',
+//     NoCurrentGameIdClaimForUser = 'NoCurrentGameIdClaimForUser',
+//     UnauthorizedRole = 'UnauthorizedRole',
+//     TeamAlreadyLoggedIn = 'TeamAlreadyLoggedIn',
+//     InvalidItemNavigation = 'InvalidItemNavigation',
+//     ValidationError = 'ValidationError',
+//     /* Success codes */
+//     Ok = 'Ok',
+//     ThatsYou = 'ThatsYou',
+//     LoggedOut = 'LoggedOut'
+// }
